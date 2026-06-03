8f73c4a6c9332ebbbd5f3b5a78f8413e5c95357e
Psychodes254 <justusmasika80@gmail.com>
2026-06-04 00:42:48 +0300

    Added the Address Class in Person's attributes

--- EclipseWorkspace/Lab1/src/Person.java
+++ EclipseWorkspace/Lab1/src/Person.java
@@ -5,18 +5,40 @@ class Person {
   String name;
   int age;
   String gender;
+  Address address;
 
   //the constructor
-  Person(String name, int age, String gender) {
+  Person(String name, int age, String gender, Address address) {
     this.name = name;
     this.age = age;
     this.gender = gender;
+    this.address = address;
   }
 }
 
-//examples and tests for Person
+// to represent Address
+class Address {
+  String city;
+  String state;
+  
+  // the constructor 
+  Address(String city, String state) {
+    this.city = city;
+    this.state = state;
+  }
+}
+
+// examples and tests for Person
 class ExamplesPerson {
-  Person tim = new Person("Tim", 23, "Male");
-  Person kate = new Person("Kate", 22, "Female");
-  Person rebecca = new Person("Rebecca", 31, "Non-binary");
+  Address atim = new Address("Boston", "MA");
+  Address akate = new Address("Warwick", "RI");
+  Address arebecca = new Address("Nashua", "NH");
+  Address amash = new Address("Busia", "NM");
+  Address ajustus = new Address("Machakos", "AR");
+  
+  Person tim = new Person("Tim", 23, "Male", atim);
+  Person kate = new Person("Kate", 22, "Female", akate);
+  Person rebecca = new Person("Rebecca", 31, "Non-binary", arebecca);
+  Person mash = new Person("Mash", 25, "Male", amash);
+  Person justus = new Person("Justus", 28, "Male", ajustus);
 }
